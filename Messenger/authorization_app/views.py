d66239ec9da4d83562d1eaa3f3b3c806e4cb0361
Dmitriy <dmitriypechenyuk0@gmail.com>
2025-05-08 00:02:22 +0300

    rework registration n authorization

--- Messenger/authorization_app/views.py
+++ Messenger/authorization_app/views.py
@@ -1,39 +1,10 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
-from django.views.generic.edit import FormMixin
+from django.views.generic.edit import FormView
 from django.contrib.auth import login
 from django.contrib.auth.models import User
 from .forms import AuthorizationForm
 # Create your views here.
 
-class AuthorizationView(TemplateView, FormMixin):
+class AuthorizationView():
     template_name = "authorization_app/authorization.html"
     form_class = AuthorizationForm
-
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context['auth_form'] = AuthorizationForm()
-        return context
-    
-    def post(self, request):
-        form = self.get_form()
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        print(email, password)
-        if form.is_valid():
-            try:
-                user = User.objects.get(email=email)
-                if user.check_password(password):
-                    
-                    login(request, user)
-
-                else:
-                    user = None
-            except User.DoesNotExist:
-                user = None
-        return render(request, self.template_name, {'auth_form': AuthorizationForm()})
-
-        
-            
-    
-
--- Messenger/registration_app/views.py
+++ Messenger/registration_app/views.py
@@ -1,34 +1,26 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
-from django.views.generic.edit import FormMixin 
+from django.views.generic.edit import FormView
 from django.contrib.auth.models import User
 from .forms import RegistrationForm
-import secrets, string
 from django.core.mail import send_mail
+import secrets, string
 # Create your views here.
 
-class RegistrationView(FormMixin ,TemplateView):
+class RegistrationView(FormView):
     template_name='registration_app/registration.html'
     form_class = RegistrationForm
+    success_url='/'    
+    def form_valid(self, form):
+        email=form.cleaned_data['email']
+        code = ''
+        for number in range(6):
+            code += secrets.choice(string.digits)
 
-    def get_context_data(self, **kwargs):
-        context=super().get_context_data(**kwargs)
-        context['reg_form'] = RegistrationForm()
-        return context
+        send_mail(
+            'Підтвердіть Електронну Адресу',
+            f'Дякуємо що користуєтесь World IT Messenger!\nКод підтвердження реєстрації: {code}',
+            None,
+            [email],
+        )
+        return super().form_valid(form)
     
-    def post(self, request):
-        form = self.get_form()
-        if form.is_valid():
-            
-            email=request.POST.get('email')
-            code = ''
-            for number in range(6):
-                code += secrets.choice(string.digits)
-
-            send_mail(
-                'Код підтвердження реєстрації',
-                f'Ваш код: {code}',
-                None,
-                [email],
-            )
-        return render(request, self.template_name, {'reg_form': RegistrationForm()})

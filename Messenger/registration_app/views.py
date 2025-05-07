0cc77c20187c7afcae17b9b6e515be82d4fdbe09
Dmitriy <dmitriypechenyuk0@gmail.com>
2025-05-07 18:36:59 +0300

    edit registration

--- Messenger/registration_app/views.py
+++ Messenger/registration_app/views.py
@@ -3,9 +3,11 @@ from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormMixin 
 from django.contrib.auth.models import User
 from .forms import RegistrationForm
+import secrets, string
+from django.core.mail import send_mail
 # Create your views here.
 
-class RegistrationView(FormMixin, TemplateView):
+class RegistrationView(FormMixin ,TemplateView):
     template_name='registration_app/registration.html'
     form_class = RegistrationForm
 
@@ -17,9 +19,16 @@ class RegistrationView(FormMixin, TemplateView):
     def post(self, request):
         form = self.get_form()
         if form.is_valid():
-            user = User.objects.create_user(
-                email=request.POST.get('email'),
-                username=request.POST.get('username'),
-                password=request.POST.get('password')
+            
+            email=request.POST.get('email')
+            code = ''
+            for number in range(6):
+                code += secrets.choice(string.digits)
+
+            send_mail(
+                'Код підтвердження реєстрації',
+                f'Ваш код: {code}',
+                None,
+                [email],
             )
         return render(request, self.template_name, {'reg_form': RegistrationForm()})
